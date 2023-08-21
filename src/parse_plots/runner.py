97fd5b8edda87da04a1f403821cc0f31769be25e
Adam Amster <adam.amster@alleninstitute.org>
2023-08-21 07:45:43 -0400

    debug

--- src/parse_plots/runner.py
+++ src/parse_plots/runner.py
@@ -353,25 +353,21 @@ class ParsePlotsRunner(argschema.ArgSchemaParser):
                         plot_point_values.append(plot_val)
                 plot_points.append(plot_point_values)
 
-            ######
-            # DEBUG
-            return
-            ######
             if plot_types[file_id] == 'dot':
                 plot_points = self._get_dot_values(
                     tick_labels=tick_labels[file_id],
                     plot_points=plot_points
                 )
 
-            elif self._is_histogram(
-                plot_type=plot_types[file_id],
-                tick_labels=tick_labels[file_id],
-                plot_points=plot_points
-            ):
-                plot_points = self._get_histogram_values(
-                    tick_labels=tick_labels[file_id],
-                    plot_points=plot_points
-                )
+            # elif self._is_histogram(
+            #     plot_type=plot_types[file_id],
+            #     tick_labels=tick_labels[file_id],
+            #     plot_points=plot_points
+            # ):
+            #     plot_points = self._get_histogram_values(
+            #         tick_labels=tick_labels[file_id],
+            #         plot_points=plot_points
+            #     )
             file_id_plot_points_map[file_id] = plot_points
         return file_id_plot_points_map
 
